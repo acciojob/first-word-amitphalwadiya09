854a8b83017f928e541df5379d1d5d08d7185c5f
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-19 19:25:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+	let str=""
+	for(let i=0;i<s.length;i++)
+		{
+			if(s.charAt(i)==" ")
+			{
+				break;
+			}
+			str=str+s.chartAt(i);
+		}
+	return str;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
